aab026dd2e46e935e74b0b4c38a0c596c3f53965
Raphael Alberto Santos dos Anjos <45611743+raphael-anjos@users.noreply.github.com>
2023-08-14 12:09:42 -0300

    Projetos Python
    Atualização de script caixa_eletronico_v2

--- Projetos/caixa_eletronico/caixa_eletronico_v2.py
+++ Projetos/caixa_eletronico/caixa_eletronico_v2.py
@@ -30,59 +30,63 @@ saques = []
 # loop infinito que vai ser executado até o usuário digitar a opção correspondente a sair
 while True:
     print(menu)
-    opcao = input("Digite a letra correspondente a opção que deseja executar: ")
+    print("Digite a letra correspondente a opção que deseja executar\n")
+    opcao = input("=> ")
     
     # deposito
     if opcao == "d":
         print(" *** DEPÓSITO ****")
-    
+
+        
         valor_deposito = float(input("Informe valor do depósito: "))
-        saldo_conta += valor_deposito
-        depositos.append(valor_deposito)
-        print (F"Depósito realizado com sucesso, o valor R$ {valor_deposito} já está disponível na sua conta")
-        time.sleep(3)
-        print("Redirecionando usuário...")
+        if valor_deposito > 0:
+                  saldo_conta += valor_deposito
+                  depositos.append(valor_deposito)
+                  print (f"Depósito realizado com sucesso, o valor R$ {valor_deposito:.2f} já está disponível na sua conta")
+                  time.sleep(3)
+                  print("Redirecionando usuário...")
+        else:
+            print("Valor inválido, tente novamente.")
+            continue    
 
     # saque
     if opcao == "s":
-        
         print(" *** SAQUE ****")
 
         if saque_diario < LIMITE_SAQUE:
-            valor_saque = float(input("Informe valor do saque:"))
-            # Loop que é executado até que usuário digite um valor menor ou igual a 500.
-            while valor_saque > 500:
-                
-                print("O limite máximo para esta operação é R$ 500,00")
-                time.sleep(3)
-                print("Redirecionando usuário...")
-                valor_saque = float(input("Informe valor do saque dentro do limite:"))
-                if valor_saque <= 500:
-                    continue # se verdadeiro exibe mensagem que a operação foi realizada com sucesso
+                valor_saque = float(input("Informe valor do saque: "))
+                # Loop que é executado até que usuário digite um valor menor ou igual a 500.
+                if valor_saque > 0:
+                    while valor_saque > 500:
+                          print("O limite máximo para esta operação é R$ 500,00")
+                          time.sleep(3)
+                          print("Redirecionando usuário...")
+                          valor_saque = float(input("Informe valor do saque dentro do limite: "))
+                          if valor_saque <= 500:
+                            continue # se verdadeiro exibe mensagem que a operação foi realizada com sucesso
+                          else:
+                            continue  # retorna para trecho onde é solicitado um valor menor ou igual 500.
+            
+                    if valor_saque > saldo_conta:
+                        print("Saldo insuficiente !")
+                        time.sleep(3)
+                        print("Redirecionando usuário...")
+                        continue
+                    else:
+                        print(f"Saque de R$ {valor_saque:.2f} realizado com sucesso, aguarde a contagem para retirar o valor.")
+                        time.sleep(3)
+                        saques.append(valor_saque)
+                    saque_diario +=1 # variavel que recebe quantos saques foram realizados
+                    saldo_conta -= valor_saque
                 else:
-                    continue  # retorna para trecho onde é solicitado um valor menor ou igual 500.
-            if valor_saque > saldo_conta:
+                     print("Valor inválido, tente novamente.")    
                 
-                print("Saldo insuficiente !")
-                time.sleep(3)
-                print("Redirecionando usuário...")
-                continue
-                
-            else:
-                
-                print("Saque realizado com sucesso, aguarde a contagem para retirar o valor.")
-                time.sleep(3)
-                saques.append(valor_saque)
-            saque_diario +=1 # variavel que recebe quantos saques foram realizados
-            saldo_conta -= valor_saque
 
         else:
-            
             print("O limite diário para executar esta operação foi atingido, por favor tente novamente outro dia")
             time.sleep(5)
             print("Redirecionando usuário...")
             continue
-            
                 
     if opcao == "e":
             print(f'''
@@ -97,14 +101,14 @@ while True:
                 \n''')
             for numero_depositos, valor_deposito in enumerate(depositos, start=1):
                 print("\n***** DEPÓSITOS ******")
-                print(f"Deposito {numero_depositos}, valor R$ {valor_deposito}")
+                print(f"Deposito {numero_depositos}, valor R$ {valor_deposito:.2f}")
         
             for saque_diario, valor_saque in enumerate(saques, start=1):
                 print("\n***** SAQUES ******")
-                print(f"Saque {saque_diario}, valor {valor_saque}")
+                print(f"Saque {saque_diario}, valor R${valor_saque:.2f}")
 
-            print(f"\n ***** SALDO DA CONTA ******")
-            print(f"Saldo da conta R$ {saldo_conta}")
+            print(f"\n***** SALDO DA CONTA ******")
+            print(f"Saldo da conta R$ {saldo_conta:.2f}")
               
             time.sleep(10)
             print("Redirecionando usuário...")
